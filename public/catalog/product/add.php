1fcf47b7d90dd7d38405d32cb0f213de4fd645c9
ssidorov <ssidorov@samsonpost.ru>
2021-02-18 19:41:34 +0300

    step-5 добавление сущности

--- public/catalog/product/add.php
+++ public/catalog/product/add.php
@@ -1,3 +1,19 @@
 <?php
 declare(strict_types=1);
 
+use App\Catalog\Product\Application\AddProduct\AddProductCommand;
+use App\Catalog\Product\Application\AddProduct\AddProductCommandHandler;
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php';
+
+if (!empty($_POST)) {
+    $command = new AddProductCommand();
+    $command->code = (int)$_POST['CODE'];
+    $command->name = $_POST['NAME'];
+    $command->brand = $_POST['BRAND'];
+    $command->price = (float)$_POST['PRICE'];
+    $command->description = $_POST['DESCRIPTION'];
+
+    $handler = app()->getContainer()->get(AddProductCommandHandler::class);
+    $handler->handle($command);
+}
