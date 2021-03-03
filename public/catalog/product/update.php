5d1e1a858aec1698d0a45067aaf0661812db08cf
ssidorov <ssidorov@samsonpost.ru>
2021-03-03 23:11:44 +0300

    step-6.1

---
+++ public/catalog/product/update.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+use App\Catalog\Product\Application\UpdateProduct\UpdateProductCommand;
+use App\Catalog\Product\Application\UpdateProduct\UpdateProductCommandHandler;
+use App\Catalog\Product\Domain\Exception\ProductNotFound;
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php';
+
+if (!empty($_POST)) {
+    $command = new UpdateProductCommand();
+    $command->id = (int)$_GET['ID'];
+    $command->name = $_POST['NAME'];
+    $command->brand = $_POST['BRAND'];
+    $command->price = (float)$_POST['PRICE'];
+    $command->description = $_POST['DESCRIPTION'];
+
+    $handler = app()->getContainer()->get(UpdateProductCommandHandler::class);
+    try {
+        $handler->handle($command);
+    } catch (InvalidArgumentException $e) {
+        echo $e->getMessage();
+    } catch (ProductNotFound $e) {
+        echo "Товар не найден";
+    } catch (\Throwable $e) {
+        // не надо так :(
+    }
+}
