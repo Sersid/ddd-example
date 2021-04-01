6d42731f1b1f7fc4050d6f386f073758e92561a6
ssidorov <ssidorov@samsonpost.ru>
2021-04-01 21:18:46 +0300

    step-11

--- public/catalog/product/template.php
+++ public/catalog/product/template.php
@@ -9,16 +9,14 @@ use App\Catalog\Product\Domain\ReadModel\ProductView;
 <h1><?= $product->name() ?></h1>
 <p><a href="<?= $product->url() ?>"><?= $product->code() ?></a></p>
 <?php if ($product->hasDescription()) { ?>
-    <div>
-        <h4>Описание</h4>
-        <p><?= $product->description() ?></p>
-    </div>
+  <div>
+    <h4>Описание</h4>
+    <p><?= $product->description() ?></p>
+  </div>
 <?php } ?>
 <?php if ($product->hasBrand()) { ?>
-    <div>
-        <h4>Бренд</h4>
-        <p>
-            <a href="<?= $product->brand()->url() ?>"><?= $product->brand()->name() ?></a>
-        </p>
-    </div>
+  <div>
+    <h4>Бренд</h4>
+    <p><a href="<?= $product->brand()->url() ?>"><?= $product->brand()->name() ?></a></p>
+  </div>
 <?php } ?>
